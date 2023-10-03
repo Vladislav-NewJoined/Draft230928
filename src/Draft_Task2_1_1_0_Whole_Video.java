d77cfa74e87d1c6513d266511375677c3cdffe8f
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-10-03 13:39:35 +0300

    IInitial commit

--- src/Draft_Task2_1_1_0_Whole_Video.java
+++ src/Draft_Task2_1_1_0_Whole_Video.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Draft_Task2_1_1_0_Whole_Video {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/Draft_Task2_1_1_1_AnonimnyeClassy.java
+++ src/Draft_Task2_1_1_1_AnonimnyeClassy.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Draft_Task2_1_1_1_AnonimnyeClassy {
+public class Draft_Task2_1_1_1_AnonimnyeClassy {
 }
