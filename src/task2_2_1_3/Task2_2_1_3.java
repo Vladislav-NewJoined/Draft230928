473c069b6ab9292642db153178d1b44c56fe45bb
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-11 09:51:16 +0300

    IInitial commit

--- src/task2_2_1_3/Task2_2_1_3.java
+++ src/task2_2_1_3/Task2_2_1_3.java
@@ -1,8 +1,7 @@
-// видеоурок прервался на мин 22 40
 package task2_2_1_3;
 
 public class Task2_2_1_3 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws InterruptedException {
         System.out.println("""
                 Задание:\s
                 Модуль 2. Тема 2. Урок 1. Многопоточность, Thread.
@@ -10,5 +9,33 @@ public class Task2_2_1_3 {
 
                 Решение:\s""");
 
+        System.out.println("СПОСОБ 1:");
+        System.out.println("Main thread started (Способ 1)...");
+        for (int i = 0; i < 5; i++) {
+            Thread t = new Thread(new MyThreadLogic());
+            t.start();
+            Thread.sleep(300);
+        }
+        System.out.println("Main thread finished (Способ 1)...");
+        System.out.println();
+
+        System.out.println("СПОСОБ 2:");
+        System.out.println("Main thread started (Способ 2)...");
+        for (int i = 0; i < 5; i++) {
+            Thread t2 = new Thread(() -> {
+                System.out.println("Способ 2");
+            },"Thread");
+            t2.start();
+            Thread.sleep(300);
+        }
+        System.out.println("Main thread finished (Способ 2)...");
+    }
+}
+
+class MyThreadLogic implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Способ 1");
     }
 }
