a18d8f05e56831d2125141d16f9ef0e5b560a4fc
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-12 13:58:06 +0300

    IInitial commit

---
+++ src/task1_3_Case3_Poshagovyi_Tetris_OSNOVNOI/Main_copy6_With_score.java
@@ -0,0 +1,4 @@
+package task1_3_Case3_Poshagovyi_Tetris_OSNOVNOI;
+
+public class Main_copy6_With_score {
+}
---
+++ src/task1_3_Case3_Poshagovyi_Tetris_OSNOVNOI/Main_copy6_Without_score.java
@@ -0,0 +1,4 @@
+package task1_3_Case3_Poshagovyi_Tetris_OSNOVNOI;
+
+public class Main_copy6_Without_score {
+}
