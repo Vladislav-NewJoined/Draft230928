f51b8b0bd275462d5f5f680a6aae0ffc15c14ac8
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-13 21:03:51 +0300

    IInitial commit

--- src/task2_2_2_1/Task2_2_2_1_copy4.java
+++ src/task2_2_2_1/Task2_2_2_1_copy4.java
@@ -1,5 +1,8 @@
 package task2_2_2_1;
 
+import java.util.Scanner;
+
+// Из разбора домашнего задания
 public class Task2_2_2_1_copy4 {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("""
@@ -10,28 +13,89 @@ public class Task2_2_2_1_copy4 {
 
                 Решение:\s""");
 
-        Thread t = new Thread(new MyThread4());
-        t.start();
+//        Thread t = new Thread(new MyThread4());
+//        t.start();
+
+        TimerState state = new TimerState();
+        new TimerThread(state).start();
+        new inputThread(state).start();
 
     }
 }
 
-class MyThread4 implements Runnable {
+//class MyThread4 implements Runnable {
+//
+//    int count = 0;
+//
+//    @Override
+//    public void run() {
+//        System.out.println("РЕАЛИЗУЕМ СЕКУНДОМЕР В ПОТОКОВОМ МЕТОДЕ run: ");
+////        while (!Thread.currentThread().interrupted()) {
+//        while (!Thread.interrupted()) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            count++;
+//            System.out.println(count + " сек.");
+//        }
+//    }
+//}
 
-    int count = 0;
+
+class TimerState {
+    int counter = 0;
+    boolean isRunning = true;
+
+    void increment() {
+        System.out.println(counter++);
+    }
+
+    void stopTime() {
+        isRunning = false;
+    }
+}
+
+
+class TimerThread extends Thread {
+    final TimerState state;
+
+    TimerThread(TimerState state) {
+        this.state = state;
+    }
+
 
     @Override
     public void run() {
         System.out.println("РЕАЛИЗУЕМ СЕКУНДОМЕР В ПОТОКОВОМ МЕТОДЕ run: ");
-//        while (!Thread.currentThread().interrupted()) {
-        while (!Thread.interrupted()) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        super.run();
+        try {
+            while (state.isRunning) {
+                state.increment();
+                sleep(1000);
             }
-            count++;
-            System.out.println(count + " сек.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class inputThread extends Thread {
+    final TimerState state;
+
+    inputThread(TimerState state) {
+        this.state = state;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("РЕАЛИЗУЕМ ПРЕРЫВАНИЕ ПОТОКА: ");
+        super.run();
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        if (s != null) {
+            state.stopTime();
         }
     }
 }
