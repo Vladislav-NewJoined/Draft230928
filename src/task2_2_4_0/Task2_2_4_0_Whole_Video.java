78609a88befa7b9a6d757277ffa9e141a065ce4d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-14 20:30:21 +0300

    IInitial commit

--- src/task2_2_4_0/Task2_2_4_0_Whole_Video.java
+++ src/task2_2_4_0/Task2_2_4_0_Whole_Video.java
@@ -1,4 +1,40 @@
 package task2_2_4_0;
 
+import java.util.concurrent.Semaphore;
+
 public class Task2_2_4_0_Whole_Video {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i < 10; i++) {
+            new MyThread(semaphore).start();
+        }
+    }
+}
+
+class MyThread extends Thread {
+    final Semaphore semaphore;
+
+    MyThread(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+//            e.printStackTrace();
+        }
+        System.out.println(getName() + " started...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+//            e.printStackTrace();
+        }
+        System.out.println(getName() + " finished...");
+        semaphore.release();
+    }
 }
