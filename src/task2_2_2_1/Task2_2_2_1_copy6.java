3ce5ca8346fff15a83ee2fd77c40ae3cc3acc437
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-14 10:07:26 +0300

    IInitial commit

--- src/task2_2_2_1/Task2_2_2_1_copy6.java
+++ src/task2_2_2_1/Task2_2_2_1_copy6.java
@@ -1,6 +1,8 @@
 package task2_2_2_1;
+
 import java.util.Scanner;
-// Из разбора домашнего задания _ // закончил на мин
+
+// Из разбора домашнего задания _ // закончил на мин 7 08
 public class Task2_2_2_1_copy6 {
 
     public static void main(String[] args) throws InterruptedException {
@@ -9,27 +11,58 @@ public class Task2_2_2_1_copy6 {
 
 }
 
+class TimerState6 {
+    int counter = 0;
+    boolean isRunning = true;
+
+    void increment() {
+        System.out.println(counter++);
+
+    }
+
+    void stopTimer() {
+        isRunning = false;
+    }
+}
+
 
 class TimerThread6 extends Thread {
-    int counter = 0;
+    final TimerState6 state;
+
+    TimerThread6(TimerState6 state) {
+        this.state = state;
+    }
+
 
     @Override
     public void run() {
         super.run();
-        while (!isInterrupted()) {
-            System.out.println(counter++);
+        try {
+            while (state.isRunning) {
+                state.increment();
+                sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
 
 class InputThread6 extends Thread {
+    final TimerState6 state;
 
+    InputThread6(TimerState6 state) {
+        this.state = state;
+    }
 
     @Override
     public void run() {
         super.run();
-        while (!isInterrupted()) {
-//            System.out.println(counter++);
+        Scanner in = new Scanner(System.in);
+
+        String s = in.nextLine();
+        if (s != null) {
+            state.stopTimer();
         }
     }
 }
