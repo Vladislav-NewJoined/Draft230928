59f8b446c8db85468154d689604b8af36bd81250
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-11-23 20:42:39 +0300

    IInitial commit

---
+++ src/task2_3_1_0/Task2_3_1_0_Whole_Video.java
@@ -0,0 +1,7 @@
+package task2_3_1_0;
+
+public class Task2_3_1_0_Whole_Video {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/task2_3_1_3_copy2/Main.java
@@ -0,0 +1,5 @@
+package task2_3_1_3_copy2;
+
+public class Main {
+
+}
