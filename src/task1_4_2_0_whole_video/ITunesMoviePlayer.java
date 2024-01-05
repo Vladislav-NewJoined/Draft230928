810e25a2a9c0ebd263d6bd249e9d7b7c0bfe1b8c
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2024-01-05 21:15:25 +0300

    IInitial commit

--- src/task1_4_2_0_whole_video/ITunesMoviePlayer.java
+++ src/task1_4_2_0_whole_video/ITunesMoviePlayer.java
@@ -1,52 +1,81 @@
-//package task1_4_2_0_whole_video;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.net.MalformedURLException;
-//import java.net.URL;
-//
-//import static javax.swing.text.html.HTML.getTag;
-//
-//public class ITunesMoviePlayer {
-////    void playMovie (String searchRequest) throws IOException {
-////        playSongInternal(searchRequest, 1);
-////    }
-////
-//// //    void playSong (String searchRequest, int limit) throws IOException {
-//// //        playSongInternal(searchRequest, limit);
-//// //    }
-//
-//    void playMovie(String searchRequest) {
-//        String url = buildUrl(searchRequest);
-//        String page = downLoadWebPage(url);
-//
-//        String artistName = getTag(page, "artistName");
-//        String trackName = getTag(page, "trackName");
-//        String previewUrl = getTag(page, "previewUrl");
-//        try(InputStream in = new URL(previewUrl).openStream()) {
-//
-//        } catch (MalformedURLException e) {
-//            throw new RuntimeException(e);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//
-//
-//
-//
-//
-//    private String getTag(String page, String artistName) {
-//        return page;
-//    }
-//
-//
-//    private String downLoadWebPage(String url) {
-//        return null;
-//    }
-//
-//    private String buildUrl(String searchRequest, int limit) {
-//        return searchRequest;
-//    }
-//}
+package task1_4_2_0_whole_video;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+// Давайте напишем заново
+
+public class ITunesMoviePlayer {
+    void playSong(String searchRequest) throws IOException {
+        playSongInternal(searchRequest, 1);
+    }
+    // конец источника: https://www.javatpoint.com/how-to-open-a-file-in-java
+
+    void playSong(String searchRequest, int limit) throws IOException {
+        playSongInternal(searchRequest, limit);
+    }
+
+    private void playSongInternal(String searchRequest, int limit) throws IOException {
+        String url = buildUrl(searchRequest, limit);
+        String page = downloadWebPage(url);
+
+        String artistName = getTag(page, "artistName");
+        String trackName = getTag(page, "trackName");
+        String previewUrl = getTag(page, "previewUrl");
+//        System.out.println(page);
+        System.out.println(artistName + " - " + trackName);
+        try (InputStream in = new URL(previewUrl).openStream()) {
+            Files.copy(in, Paths.get(trackName + ".m4a"));
+        }
+        System.out.println("Downloaded!");
+
+        // источник: https://www.javatpoint.com/how-to-open-a-file-in-java
+        if (!Desktop.isDesktopSupported()) //check if Desktop is supported by Platform or not
+        {
+            System.out.println("File opening not supported");
+            return;
+        }
+
+        Desktop desktop = Desktop.getDesktop();
+        File file = new File(trackName + ".m4a");
+        desktop.open(file); //opens the specified file
+    }
+
+    private String getTag(String page, String tagName) {
+//        int start = page.indexOf("artistName") + "artistName".length() + 3;
+        int start = page.indexOf(tagName) + tagName.length() + 3;
+        int end = page.indexOf("\"", start);
+        String value = page.substring(start, end);
+        return value;
+    }
+
+    private String buildUrl(String searchRequest, int limit) {
+        String term = searchRequest.replaceAll(" ", "+");
+        String itunesApi = "https://itunes.apple.com/search?term=";
+        String limitParam = "&limit=1" + limit;
+        String mediaParam = "&media=music";
+        StringBuilder builder = new StringBuilder();
+        builder.append(itunesApi);
+        builder.append(term);
+        builder.append(limitParam);
+        builder.append(mediaParam);
+        return builder.toString();
+    }
+
+    static String downloadWebPage(String url) throws IOException {
+        StringBuilder result = new StringBuilder();
+        String line;
+        URLConnection urlConnection = new URL(url).openConnection();
+        try (InputStream is = urlConnection.getInputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+        }
+        return result.toString(); // goto 13 line
+    }
+}
